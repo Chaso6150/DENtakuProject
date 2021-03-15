6411181ceb74abc81e2edc77be7704d5cb8c97e8
Chaso6150 <kiyujokel74.56ra@gmail.com>
2021-03-15 18:28:43 +0900

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ window.onload = function () {
 
     // オブジェクトと変数の準備
     var count1_disp = document.getElementById("count1");
+    var count1_now = document.getElementById("count1.1");
     var count1_up_btn = document.getElementById("id_1");
     var count2_disp = document.getElementById("count2");
     var count2_up_btn = document.getElementById("id_2");
@@ -11,34 +12,62 @@ window.onload = function () {
     var count1_value = 0;
     var count2_value = 0;
     var count3_value = 0;
-
+    var flag = [];
+    for (let i = 0; i < 100; i++) {
+        flag.push(0);
+    }
+    console.log(`content: ${flag}, length: ${flag.length}`)
         // カウントアップボタンクリック処理
-        count1_up_btn.onclick = function () {
-            count1_value += 1;
+    count1_up_btn.onclick = function () {
+        if (flag[1] === 0) {
+            count1_value += 2;
             count1_disp.innerHTML = count1_value;
+            count1_now.innerHTML = 98 - count1_value;
+            flag[1] = 1;
+        } else {
+            count1_value -= 2;
+            count1_disp.innerHTML = count1_value;
+            count1_now.innerHTML = 98 - count1_value;
+            flag[1] = 0;
         }
+    }
+    
         // カウントアップボタンのマウスダウン処理
-        count1_up_btn.onmousedown = function () {
+    count1_up_btn.onmousedown = function () {
+        if (flag[1] === 0) {
             count1_up_btn.style.backgroundColor = "#00FF00";
-        }
-        // カウントアップボタンのマウスアップ処理
-        count1_up_btn.onmouseup = function () {
+        } else {
             count1_up_btn.style.backgroundColor = "";
         }
+    }
 
-    // カウントアップボタンクリック処理
-    count2_up_btn.onclick = function () {
-        count2_value += 2;
-        count2_disp.innerHTML = count2_value;
-    }
-    // カウントアップボタンのマウスダウン処理
+            // カウントアップボタンクリック処理
+            count2_up_btn.onclick = function () {
+                if (flag[2] === 0) {
+                    count1_value += 2;
+                    count1_disp.innerHTML = count1_value;
+                    count1_now.innerHTML = 98 - count1_value;
+                    flag[2] = 1;
+                } else {
+                    count1_value -= 2;
+                    count1_disp.innerHTML = count1_value;
+                    count1_now.innerHTML = 98 - count1_value;
+                    flag[2] = 0;
+                }
+            }
+    
+
+            // カウントアップボタンのマウスダウン処理
     count2_up_btn.onmousedown = function () {
-        count2_up_btn.style.backgroundColor = "#00FF00";
+        if (flag[2] === 0) {
+            count2_up_btn.style.backgroundColor = "#00FF00";
+        } else {
+            count2_up_btn.style.backgroundColor = "";
+        }
     }
-    // カウントアップボタンのマウスアップ処理
-    count2_up_btn.onmouseup = function () {
-        count2_up_btn.style.backgroundColor = "";
-    }
+                
+
+
 
     // カウントアップボタンクリック処理
     count3_up_btn.onclick = function () {
